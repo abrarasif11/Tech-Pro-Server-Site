4feead0dada4499b7b6e72193f176a4f746551c2
abrarasif11 <faasif929@gmail.com>
2022-10-25 11:40:57 +0600

    courses-data-added

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const express = require('express')
+const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('News API Running');
+app.use(cors());
+
+
+const courses = require("./data/learning.json");
+
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
 });
+
+
+app.get("/", (req, res) => {
+  res.send("Learn With Us");
+});
+
 app.listen(port, () => {
-    console.log('Learning Course Data', port);
-})
+  console.log("Learn With Us:", port);
+});
+
